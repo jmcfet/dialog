40d48e77f4bf5f5f0936589f2dfc7b658613f051
john mcfetridge <jmcfet@bellsouth.net>
2017-07-06 19:15:21 -0400

    Add project files.

---
+++ dialog3/Dialog.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace dialog3
+{
+    public class OnSignUpEventArgs : EventArgs
+    {
+        string mEmail;
+        string mPassword;
+        public string EMail
+        {
+            get { return mEmail; }
+            set { mEmail = value; }
+        }
+        public string Password
+        {
+            get { return mPassword; }
+            set { mPassword = value; }
+        }
+        public OnSignUpEventArgs(string email, string password) : base()
+        {
+            mEmail = email;
+            mPassword = password;
+        }
+    }
+    class DialogClass : DialogFragment
+    {
+        EditText txtEmail;
+        EditText txtPassword;
+        Button btnSignup;
+        public event EventHandler<OnSignUpEventArgs> mOnsignupDone;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            var view = inflater.Inflate(Resource.Layout.signup, container, false);
+            txtEmail = view.FindViewById<EditText>(Resource.Id.email);
+            txtPassword = view.FindViewById<EditText>(Resource.Id.password);
+            btnSignup = view.FindViewById<Button>(Resource.Id.signup);
+            btnSignup.Click += BtnSignup_Click;
+            return view;
+
+        }
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+     //       Dialog.Window.RequestFeature(WindowFeatures.NoTitle);
+            base.OnActivityCreated(savedInstanceState);
+            //       Dialog.Window.Attributes.WindowAnimations = Resource.
+        }
+
+        private void BtnSignup_Click(object sender, EventArgs e)
+        {
+            mOnsignupDone.Invoke(this, new OnSignUpEventArgs(txtEmail.Text, txtPassword.Text));
+            this.Dismiss();
+        }
+    }
+}
---
+++ dialog3/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace dialog3
+{
+    [Activity(Label = "dialog3", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button mButtnSignup;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            mButtnSignup = FindViewById<Button>(Resource.Id.sinup);
+            mButtnSignup.Click += (object sender, EventArgs args) =>
+            {
+                FragmentTransaction transcation = FragmentManager.BeginTransaction();
+                DialogClass signup = new DialogClass();
+                signup.Show(transcation, "Dialog Fragment");
+                signup.mOnsignupDone += Signup_mOnsignupDone;
+            };
+        }
+
+        private void Signup_mOnsignupDone(object sender, OnSignUpEventArgs e)
+        {
+    //        throw new NotImplementedException();
+        }
+    }
+}
+
